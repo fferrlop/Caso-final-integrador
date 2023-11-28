90e8095936f2ed72a0e29e3b0062b04bc1f25b85
fferrlop <fferrlop@myuax.com>
2023-11-28 12:06:15 +0100

    Bibliotecas

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include "json11.hpp"
+#include "jsonlib.hpp"
+
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
