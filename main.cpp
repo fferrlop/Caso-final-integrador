b742e9568db67c7822eb2d528b491246451506f7
fferrlop <fferrlop@myuax.com>
2023-11-28 12:26:47 +0100

    4

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ int main() {
 
     class Variant {
     public:
-        typedef Variant(*proc_type) ( const std::vector<Variant>& );
+        typedef Variant(*proc_type)(const std::vector<Variant> &);
+
         typedef std::vector<Variant>::const_iterator iter;
         typedef std::map<std::string, Variant> map;
 
@@ -21,15 +22,21 @@ int main() {
         std::string val;
         std::vector<Variant> list;
         proc_type proc;
-        Entorno* env;
+        Entorno *env;
 
 
-        Variant(variant_type type = Symbol) : type(type) , env(0), proc(0) { }
-        Variant(variant_type type, const std::string& val) : type(type), val(val) , env(0) , proc(0) { }
-        Variant(proc_type proc) : type(Proc), proc(proc) , env(0) { }
+        Variant(variant_type type = Symbol) : type(type), env(0), proc(0) {}
+
+        Variant(variant_type type, const std::string &val) : type(type), val(val), env(0), proc(0) {}
+
+        Variant(proc_type proc) : type(Proc), proc(proc), env(0) {}
 
         std::string to_string();
+
         std::string to_json_string();
+
         static Variant from_json_string(std::string json);
+
         static Variant parse_json(jsonlib::Json job);
     }
+}
