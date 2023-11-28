5d9326801f04c296f823ebbb377120f86f368dc6
fferrlop <fferrlop@myuax.com>
2023-11-28 12:19:04 +0100

    entorno

--- main.cpp
+++ main.cpp
@@ -3,11 +3,18 @@
 #include <string>
 #include <map>
 #include "json11.hpp"
-#include "jsonlib.hpp"
 
 
+enum variant_type { Symbol, Number, List, Proc, Lambda, Cadena };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+
+    struct Entorno;
+
+    class Variant {
+    public:
+        typedef Variant(*proc_type) ( const std::vector<Variant>& );
+        typedef std::vector<Variant>::const_iterator iter;
+        typedef std::map<std::string, Variant> map;
+
+    }
